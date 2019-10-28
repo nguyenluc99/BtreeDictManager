11f6e8e6df5eea6a9f71788215d2324fe20de7c2
nguyenluc99 <yeuma99@gmail.com>
2019-10-28 17:07:34 +0700

    add lncurses library

--- miniProject.c
+++ miniProject.c
@@ -64,44 +64,44 @@ long* addAWord(BTA* dict) {
 }
 
 
-void auto_complete(){
-    char *file ="auto_complete";
-    btinit();
-    BTA *complete_tree;
-    complete_tree = btopn(file,0,0);
+// void auto_complete(){
+//     char *file ="auto_complete";
+//     btinit();
+//     BTA *complete_tree;
+//     complete_tree = btopn(file,0,0);
 
-    if(!complete_tree)
-        complete_tree= btcrt(file, 0,0);
-    FILE *f = fopen("/user/share/dict/words","r");
+//     if(!complete_tree)
+//         complete_tree= btcrt(file, 0,0);
+//     FILE *f = fopen("/user/share/dict/words","r");
 
-    char word[20];
-    char value[600];
-    int rsize;
-    while(!feof(f)){
-        fgets(word, 20, f);
-        word[strlen(word)-1]='\0';
+//     char word[20];
+//     char value[600];
+//     int rsize;
+//     while(!feof(f)){
+//         fgets(word, 20, f);
+//         word[strlen(word)-1]='\0';
 
-        int length = strlen(word);
-        for(int i=1;i<length+1; i++){
-            char key[30];
-            strncmp(key, word,1);
-            if(btsel(complete_tree,key,value,200, &rsize)){
-                int check = btins(complete_tree, key, word, 30);
-            }
-            else{
-                strcpy(value, "");
-                int check= btsel(complete_tree, key, value, sizeof(value), &rsize);
-                if(strlen(value)<500){
-                    strcat(value, "\n");
-                    strcat(value, word);
-                    btupd(complete_tree, key, value, strlen(value)+1);
-                }
-            }
-        }
-    }
-    fclose(f);
-    btcls(complete_tree);
-}
+//         int length = strlen(word);
+//         for(int i=1;i<length+1; i++){
+//             char key[30];
+//             strncmp(key, word,1);
+//             if(btsel(complete_tree,key,value,200, &rsize)){
+//                 int check = btins(complete_tree, key, word, 30);
+//             }
+//             else{
+//                 strcpy(value, "");
+//                 int check= btsel(complete_tree, key, value, sizeof(value), &rsize);
+//                 if(strlen(value)<500){
+//                     strcat(value, "\n");
+//                     strcat(value, word);
+//                     btupd(complete_tree, key, value, strlen(value)+1);
+//                 }
+//             }
+//         }
+//     }
+//     fclose(f);
+//     btcls(complete_tree);
+// }
 
 int main(){
     int n, size, i;
